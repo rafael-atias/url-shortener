45c7f5a8d7e2b9e9f7813ebef938f151ee0acd69
Rafael Atías <rafael.atias@gmail.com>
2020-04-26 21:29:12 -0300

    The API accepts only post requests

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.DB_URI);
 
 app.use(cors());
@@ -21,14 +21,14 @@ app.use(cors());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post("/api/shorturl/new", function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
 
