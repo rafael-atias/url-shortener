6fa3a6857ee6f5693549c62618f71b6749be84fc
Rafael Atías <rafael.atias@gmail.com>
2020-05-05 06:54:09 -0300

    extracted functions and constants to helpers.js

--- helpers.js
+++ helpers.js
@@ -1,5 +1,29 @@
+"use-strict";
+
+// mongo and mongoosejs related requires
+
+const mongoSanitize = require("mongo-sanitize");
+
+const mongo = require('mongodb');
+const { connect, Schema, model } = require('mongoose');
+
+// nodejs requires
+
 const DNS = require("dns");
 
+// database schema and model definitions
+
+/** this project needs a db !! **/
+connect(process.env.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+const urlSchema = new Schema({
+    original_url: { type: String, required: true, unique: true },
+    short_url: { type: String },
+});
+
+const Url = model("Url", urlSchema);
+
+// function definitions
 const isUrlLive = function (url) {
     return new Promise(function (resolve, reject) {
         const u = (typeof url === "string") ? new URL(url) : url;
@@ -18,6 +42,84 @@ const isUrlLive = function (url) {
     })
 };
 
+const getUrlFromDb = async function (response, encodedUrl) {
+    const { original_url, short_url } = await Url
+        .findOne({ original_url: encodedUrl })
+        .exec();
+
+    return response.json({
+        original_url,
+        short_url,
+    });
+};
+
+const createNewEntryInDb = async function (response, encodedUrl) {
+    const count = await Url
+        .estimatedDocumentCount()
+        .exec();
+
+    const { original_url, short_url } = await Url.create({
+        original_url: encodedUrl,
+        short_url: count + 1,
+    });
+
+    return response.json({
+        original_url,
+        short_url,
+    });
+};
+
+const newShortUrlHandler = async function (request, response) {
+    try {
+        const encodedUrl = encodeURI(request.body.url);
+
+        const bool = await isUrlLive(encodedUrl);
+
+        if (bool === false) {
+            return response.json({
+                error: "invalid URL",
+            });
+        }
+
+        if (await Url.exists({ original_url: encodedUrl })) {
+            return getUrlFromDb(response, encodedUrl);
+        }
+
+        return createNewEntryInDb(response, encodedUrl);
+
+    } catch (error) {
+        return response.json({
+            error: "We cannot communicate with the database. Please, try again later"
+        });
+    }
+};
+
+const getUrlHandler = async function (request, response) {
+    try {
+
+        if (await Url.exists({ short_url: request.params.url })) {
+            const entry = await Url
+                .findOne({ short_url: request.params.url })
+                .exec();
+
+            return response.redirect(303, entry.original_url);
+        }
+
+        throw new Error("invalid url");
+    } catch (error) {
+        return response.json({
+            error: error.message,
+        });
+    }
+};
+
+const runSanitizer = function (request, response, next) {
+    request.body = mongoSanitize(request.body);
+    next();
+};
+
 module.exports = {
-    isUrlLive,
+    newShortUrlHandler,
+    getUrlHandler,
+    runSanitizer,
 };
--- server.js
+++ server.js
@@ -3,10 +3,6 @@
 // imports
 
 const express = require('express');
-const mongoSanitize = require("mongo-sanitize");
-
-const mongo = require('mongodb');
-const { connect, Schema, model } = require('mongoose');
 
 const cors = require('cors');
 
@@ -14,23 +10,12 @@ const app = express();
 
 const bodyParser = require("body-parser");
 
-const { isUrlLive } = require("./helpers");
+const { runSanitizer, newShortUrlHandler, getUrlHandler } = require("./helpers");
 
 // Basic Configuration 
 const port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/
-connect(process.env.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
-
-const urlSchema = new Schema({
-  original_url: { type: String, required: true, unique: true },
-  short_url: { type: String },
-});
-
-const Url = model("Url", urlSchema);
-
 // middleware
-
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
@@ -44,71 +29,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/api/shorturl/new", function (request, response, next) {
-  request.body = mongoSanitize(request.body);
-  next();
-}, async function (request, response) {
-  try {
-    const bodyUrl = encodeURI(request.body.url);
+app.post("/api/shorturl/new", runSanitizer, newShortUrlHandler);
 
-    const bool = await isUrlLive(bodyUrl);
-
-    if (bool === false) {
-      return response.json({
-        error: "invalid URL",
-      });
-    }
-
-    if (await Url.exists({ original_url: bodyUrl })) {
-      const entry = await Url
-        .findOne({ original_url: bodyUrl })
-        .exec();
-
-      return response.json({
-        original_url: entry.original_url,
-        short_url: entry.short_url,
-      });
-    }
-
-    const count = await Url
-      .estimatedDocumentCount()
-      .exec();
-
-    const entry = await Url.create({
-      original_url: bodyUrl,
-      short_url: count + 1,
-    });
-
-    return response.json({
-      original_url: entry.original_url,
-      short_url: entry.short_url
-    });
-
-  } catch (error) {
-    return response.json({
-      error: "We cannot communicate with the database. Please, try again later"
-    });
-  }
-});
-
-app.get("/api/shorturl/:url", async function (request, response) {
-  try {
-
-    if (await Url.exists({ short_url: request.params.url })) {
-      const entry = await Url
-        .findOne({ short_url: request.params.url })
-        .exec();
-
-      return response.redirect(303, entry.original_url);
-    }
-
-    throw new Error("invalid url");
-  } catch (error) {
-    return response.json({
-      error: error.message,
-    });
-  }
-});
+app.get("/api/shorturl/:url", getUrlHandler);
 
 app.listen(port, function () {
   console.log(`Node.js listening on port ${port}`);
